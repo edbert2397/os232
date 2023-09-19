ae2b19cb646c43cc9a962996c6e369fcb4356eca
edbert2397 <51374764+edbert2397@users.noreply.github.com>
2023-09-19 13:34:25 +0700

    edit sshkeys

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="0942C81B375BDFDC0039D4876328DC6A4F8303F3"
+REC2="6328DC6A4F8303F3"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
